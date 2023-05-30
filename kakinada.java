6767a6b4f78be66ae56cfb375c30a87fa64868a7
sureshpadala2705 <sureshpadala83@gmail.com>
2023-05-30 16:20:15 +0530

    kakinada update

--- kakinada.java
+++ kakinada.java
@@ -1,1 +1,2 @@
-As kakinada has been city so it was devolped city so AJIO has been from last 20 years is there
+As kakinada has been city so it was devolped city so AJIO has been from last 20 years is there.
+Update-- that AJIO store in kakinada has many models
