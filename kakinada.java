6e939ef3b3e794e0fce0ea695ff7e8e89124678f
sureshpadala2705 <sureshpadala83@gmail.com>
2023-05-30 18:13:26 +0530

    updated Revert "demo2"
    This reverts commit 08cb3972d2c32e7f9bc7473ee4c3530407a48dd1.

--- kakinada.java
+++ kakinada.java
@@ -1,2 +1,2 @@
 As kakinada has been city so it was devolped city so AJIO has been from last 20 years is there.
-Update-- that AJIO store in kakinada has many models--updated for testing`
+Update-- that AJIO store in kakinada has many models
